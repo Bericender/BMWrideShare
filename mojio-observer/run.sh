c0f467e6917200e025537a264e569d22426db9a5
Justin Karneges <justin@affinix.com>
2015-01-10 21:50:07 -0800

    fix path

--- mojio-observer/run.sh
+++ mojio-observer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Djava.library.path=/usr/local/lib -cp build/jar/mojio-observer.jar:/home/justin/dev/signalr-java-client/signalr-client-sdk/build/libs/signalr-client-sdk.jar:/usr/share/java/gson.jar:/usr/local/share/java/zmq.jar MojioObserver
+java -Djava.library.path=/usr/local/lib -cp build/jar/mojio-observer.jar:/home/justin/signalr-java-client/signalr-client-sdk/build/libs/signalr-client-sdk.jar:/usr/share/java/gson.jar:/usr/local/share/java/zmq.jar MojioObserver
