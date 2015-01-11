c6fffcd40a6b6bd98b79abad0058e187981d0198
Bericender <lgxge@nottingham.ac.uk>
2015-01-11 08:48:33 -0800

    Heap of shards

--- BMWrideShare/BMWrideShare/ViewController.swift
+++ BMWrideShare/BMWrideShare/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var Frontseat: UIImageView!
+    
+    @IBAction func onStart(sender: AnyObject) {
+        self.performSegueWithIdentifier("fromStartToMainSegue", sender: self)
+        
+    }
+    @IBOutlet weak var FrontRightSeat: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +24,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func toggleFrontRight() {
+        if (self.FrontRightSeat.alpha == 0) {
+            self.FrontRightSeat.alpha = 1
+        } else {
+            self.FrontRightSeat.alpha = 0
+        }
+    }
 
 
+    @IBAction func onButton(sender: AnyObject) {
+        toggleFrontRight()
+    }
 }
 
