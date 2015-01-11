d30cda5d936bc18cbcecc044386fac4a743e5e8c
Justin Woo <justinzw@gmail.com>
2015-01-10 20:07:12 -0800

    added cocopods

---
+++ BMWrideShare/Pods/Target Support Files/Pods-BMWrideShare-CMDQueryStringSerialization/Pods-BMWrideShare-CMDQueryStringSerialization-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BMWrideShare_CMDQueryStringSerialization : NSObject
+@end
+@implementation PodsDummy_Pods_BMWrideShare_CMDQueryStringSerialization
+@end
